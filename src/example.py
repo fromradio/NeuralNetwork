833c9fb68533bdb7e06ad7c54b0cb41e7149b655
fromradio <ruimin.wang13@gmail.com>
2014-10-14 16:46:51 +0800

    update test

--- src/example.py
+++ src/example.py
@@ -4,12 +4,22 @@ import numpy as np
 
 
 # generate a set of data that can be separated by a hyper plane
-def data_generate(m,n):
-	# generate a plane
-	hyper_plane = np.random.rand(m)
+def linear_data_generate(m,n):
+	# generate a hyper plane
+	hyper_plane = np.random.randn(m+1)
+	training_data = []
 	for i in range(n):
 		# 
-		pass 
+		t = np.random.randn(m+1)
+		t[0] = 1.0
+		if np.dot(t,hyper_plane) < 0:
+			training_data.append(np.append(t[1:],-1))
+		elif np.dot(t,hyper_plane)> 0:
+			training_data.append(np.append(t[1:],1))
+		else:
+			pass
+	return training_data
 
 if __name__ == '__main__':
-	data_generate(100)
+	t = data_generate(2,10)
+	print t
